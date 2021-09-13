44ab6c663b1a08a5e01ce3714d3d77f45965e3cb
Marcelo Ohashi <mohashi@redhat.com>
2021-09-13 09:15:42 -0300

    Adding select and some fixes

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ let todoForm = document.querySelector(".todo-form");
 let cancelButton = document.querySelector("#btn-cancel");
 let saveButton = document.querySelector("#btn-save");
 let actionsAddButton = document.querySelector("#actions-add");
+let checkboxList = document.querySelectorAll(".item-checkbox");
+let actionsSubmenuButton = document.querySelector("#actions-submenu");
 
 function openMenu() {
     menu.style.display = 'block';
@@ -53,3 +55,26 @@ if (actionsAddButton) {
         openModal();
     });
 }
+
+var selected = []
+
+for (var i=0; i < checkboxList.length ; i++) {
+    checkboxList[i].addEventListener("click", event => {
+        let div = event.target.closest('.item');
+        div.classList.toggle("selected");
+
+        if (event.target.checked)
+            selected.push(event.target);
+        else
+            selected.splice(selected.indexOf(event.target));
+
+        if (selected.length > 0) {
+            actionsSubmenuButton.style.display = 'block';
+            actionsAddButton.style.display = 'none';
+        } else {
+            actionsAddButton.style.display = 'block';
+            actionsSubmenuButton.style.display = 'none';
+        }
+    });
+}
+
